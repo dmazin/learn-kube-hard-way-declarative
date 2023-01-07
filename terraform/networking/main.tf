cee9315d3d5837d6ff7963fb2a68580b23f34fdb
Dmitry Mazin <dm@archvile.net>
2023-01-07 07:56:59 +0000

    fmt

--- terraform/networking/main.tf
+++ terraform/networking/main.tf
@@ -28,33 +28,33 @@ provider "google-beta" {
 }
 
 module "vpc" {
-    source  = "terraform-google-modules/network/google//modules/vpc"
-    version = "~> 6.0.0"
+  source  = "terraform-google-modules/network/google//modules/vpc"
+  version = "~> 6.0.0"
 
-    project_id   = var.project
-    network_name = var.network_name
+  project_id   = var.project
+  network_name = var.network_name
 
-    shared_vpc_host = false
+  shared_vpc_host = false
 }
 
 module "subnets" {
-    source  = "terraform-google-modules/network/google//modules/subnets"
-    version = "~> 6.0.0"
+  source  = "terraform-google-modules/network/google//modules/subnets"
+  version = "~> 6.0.0"
 
-    project_id = var.project
-    network_name = module.vpc.network_name
+  project_id   = var.project
+  network_name = module.vpc.network_name
 
-    subnets = [
-        {
-            subnet_name = "kubernetes"
-            subnet_ip = "10.240.0.0/24"
-            subnet_region = var.region
-        }
-    ]
+  subnets = [
+    {
+      subnet_name   = "kubernetes"
+      subnet_ip     = "10.240.0.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
 
 module "firewall_rules" {
-  source       = "terraform-google-modules/network/google//modules/firewall-rules"
+  source  = "terraform-google-modules/network/google//modules/firewall-rules"
   version = "~> 6.0.0"
 
   project_id   = var.project
@@ -65,26 +65,26 @@ module "firewall_rules" {
     description             = null
     direction               = "INGRESS"
     priority                = null
-    ranges                  = [for s in module.subnets.subnets: s.ip_cidr_range]
+    ranges                  = [for s in module.subnets.subnets : s.ip_cidr_range]
     source_tags             = null
     source_service_accounts = null
     target_tags             = null
     target_service_accounts = null
     allow = [{
       protocol = "tcp"
-      ports    = null  # All
-    },{
+      ports    = null # All
+      }, {
       protocol = "udp"
-      ports    = null  # All
-    },{
+      ports    = null # All
+      }, {
       protocol = "icmp"
-      ports = null
+      ports    = null
     }]
     deny = []
     log_config = {
       metadata = "INCLUDE_ALL_METADATA"
     }
-  }, {
+    }, {
     name                    = "allow-external"
     description             = null
     direction               = "INGRESS"
@@ -97,13 +97,23 @@ module "firewall_rules" {
     allow = [{
       protocol = "tcp"
       ports    = ["22", "6443"]
-    }, {
+      }, {
       protocol = "icmp"
-      ports =  null
+      ports    = null
     }]
     deny = []
     log_config = {
       metadata = "INCLUDE_ALL_METADATA"
     }
   }]
+}
+
+module "external_address" {
+  source  = "terraform-google-modules/address/google//"
+  version = "~> 3.1.2"
+
+  project_id = var.project
+  region     = var.region
+
+  address_type = "EXTERNAL"
 }
